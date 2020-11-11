8b6a4891cbed163600efbc0f0705c441f463ad1a
Balint Reczey <balint.reczey@canonical.com>
2020-11-11 21:48:36 +0100

    arch-speed.sql: Add estimate about running each test once

--- arch-speed.sql
+++ arch-speed.sql
@@ -1,4 +1,4 @@
-SELECT 'Average test run time in seconds per architecture:';
+SELECT 'Average test run time in seconds per architecture (in Groovy):';
 SELECT t.arch, ROUND(AVG(r.d))
 FROM test t
 JOIN (SELECT test_id, AVG(duration) as d, version
@@ -12,5 +12,16 @@ JOIN (SELECT package
      WHERE release = 'groovy'
      GROUP BY package
      HAVING count(arch) = 6) AS all_arch_pkgs ON all_arch_pkgs.package = t.package
+WHERE t.release = 'groovy'
+GROUP BY t.arch;
+
+SELECT 'Total time needed to run each test once on the latest version (in hours, per architecture, in Groovy):';
+SELECT t.arch, ROUND(SUM(r.d)/3600)
+FROM test t
+JOIN (SELECT test_id, AVG(duration) as d, version
+     FROM result
+     GROUP BY test_id, version
+     HAVING version = MAX(version)) AS r ON r.test_id = t.id
+JOIN current_version c ON c.version = r.version AND c.package = t.package AND c.release = t.release
 WHERE t.release = 'groovy'
 GROUP BY t.arch;
